03bc209b60ed54d94cf589f49c36acab65073c21
Nugine <nugine@foxmail.com>
2021-03-01 11:59:34 +0800

    update resources

--- src/unix/resource_type.rs
+++ src/unix/resource_type.rs
@@ -126,7 +126,7 @@ macro_rules! declare_resource {
                     {
                         assert_eq!(Resource::$id.as_name(), stringify!($c_enum));
                         assert_eq!(Resource::from_str(stringify!($c_enum)).unwrap(), Resource::$id);
-                        assert!(libc::$c_enum >= 0 && libc::$c_enum <= 128);
+                        assert!((0..=128).contains(&libc::$c_enum));
                     }
                 )+
             }
@@ -187,14 +187,15 @@ declare_resource! {
         target_os = "fuchsia",
         any(target_os = "macos", target_os = "ios"),
         any(target_os = "freebsd", target_os = "dragonfly"),
+        target_os = "netbsd",
         target_os = "haiku",
         target_os = "android",
         target_os = "emscripten",
         all(target_os = "linux", target_env = "gnu"),
         all(target_os = "linux", target_env = "musl", any(target_arch = "x86", target_arch = "mips", target_arch = "powerpc", target_arch = "hexagon", target_arch = "arm")),
         all(target_os = "linux", target_env = "musl", any(target_arch = "x86_64", target_arch = "aarch64", target_arch = "mips64", target_arch = "powerpc64")),
+        all(target_os = "linux", target_env = "uclibc", any(target_arch = "mips", target_arch = "mips64")),
         target_os = "solarish",
-        all(target_env = "uclibc",any(target_arch = "mips", target_arch = "mips64"))
     ))]
     /// The maximum size (in bytes)
     /// of the process's virtual memory (address space).
@@ -210,8 +211,8 @@ declare_resource! {
         target_os = "emscripten",
         all(target_os = "linux", target_env = "gnu"),
         all(target_os = "linux", target_env = "musl"),
+        all(target_os = "linux", target_env = "uclibc"),
         target_os = "solarish",
-        target_env = "uclibc"
     ))]
     /// The maximum size (in bytes)
     /// of a core file that the process may dump.
@@ -227,8 +228,8 @@ declare_resource! {
         target_os = "emscripten",
         all(target_os = "linux", target_env = "gnu"),
         all(target_os = "linux", target_env = "musl"),
+        all(target_os = "linux", target_env = "uclibc"),
         target_os = "solarish",
-        target_env = "uclibc"
     ))]
     /// A limit (in seconds)
     /// on the amount of CPU time that the process can consume.
@@ -244,8 +245,8 @@ declare_resource! {
         target_os = "emscripten",
         all(target_os = "linux", target_env = "gnu"),
         all(target_os = "linux", target_env = "musl"),
+        all(target_os = "linux", target_env = "uclibc"),
         target_os = "solarish",
-        target_env = "uclibc"
     ))]
     /// The maximum size (in bytes)
     /// of the process's data segment
@@ -262,15 +263,15 @@ declare_resource! {
         target_os = "emscripten",
         all(target_os = "linux", target_env = "gnu"),
         all(target_os = "linux", target_env = "musl"),
+        all(target_os = "linux", target_env = "uclibc"),
         target_os = "solarish",
-        target_env = "uclibc"
     ))]
     /// The maximum size (in bytes)
     /// of files that the process may create.
     FSIZE = 5 => RLIMIT_FSIZE,
 
     #[cfg(any(
-        target_os = "freebsd"
+        target_os = "freebsd",
     ))]
     /// The maximum number of kqueues this user id is allowed to create.
     KQUEUES = 6 => RLIMIT_KQUEUES,
@@ -281,7 +282,7 @@ declare_resource! {
         target_os = "emscripten",
         all(target_os = "linux", target_env = "gnu"),
         all(target_os = "linux", target_env = "musl"),
-        target_env = "uclibc"
+        all(target_os = "linux", target_env = "uclibc"),
     ))]
     /// (early Linux 2.4 only)
     ///
@@ -300,7 +301,7 @@ declare_resource! {
         all(target_os = "linux", target_env = "gnu"),
         all(target_os = "linux", target_env = "musl", any(target_arch = "x86", target_arch = "mips", target_arch = "powerpc", target_arch = "hexagon", target_arch = "arm")),
         all(target_os = "linux", target_env = "musl", any(target_arch = "x86_64", target_arch = "aarch64", target_arch = "mips64", target_arch = "powerpc64")),
-        all(target_env = "uclibc",any(target_arch = "mips", target_arch = "mips64"))
+        all(target_os = "linux", target_env = "uclibc", any(target_arch = "mips", target_arch = "mips64")),
     ))]
     /// The maximum number (in bytes)
     /// of memory that may be locked into RAM.
@@ -312,7 +313,7 @@ declare_resource! {
         target_os = "emscripten",
         all(target_os = "linux", target_env = "gnu"),
         all(target_os = "linux", target_env = "musl"),
-        target_env = "uclibc"
+        all(target_os = "linux", target_env = "uclibc"),
     ))]
     /// A limit on the number
     /// of bytes that can be allocated for POSIX message queues
@@ -325,7 +326,7 @@ declare_resource! {
         target_os = "emscripten",
         all(target_os = "linux", target_env = "gnu"),
         all(target_os = "linux", target_env = "musl"),
-        target_env = "uclibc"
+        all(target_os = "linux", target_env = "uclibc"),
     ))]
     /// This specifies a ceiling
     /// to which the process's nice value can be raised
@@ -343,8 +344,8 @@ declare_resource! {
         all(target_os = "linux", target_env = "gnu"),
         all(target_os = "linux", target_env = "musl", any(target_arch = "x86", target_arch = "mips", target_arch = "powerpc", target_arch = "hexagon", target_arch = "arm")),
         all(target_os = "linux", target_env = "musl", any(target_arch = "x86_64", target_arch = "aarch64", target_arch = "mips64", target_arch = "powerpc64")),
+        all(target_os = "linux", target_env = "uclibc", any(target_arch = "mips", target_arch = "mips64")),
         target_os = "solarish",
-        all(target_env = "uclibc",any(target_arch = "mips", target_arch = "mips64"))
     ))]
     /// This specifies a value
     /// one greater than the maximum file descriptor number
@@ -352,7 +353,7 @@ declare_resource! {
     NOFILE = 11 => RLIMIT_NOFILE,
 
     #[cfg(any(
-        target_os = "haiku"
+        target_os = "haiku",
     ))]
     /// The number of open vnode monitors.
     NOVMON = 12 => RLIMIT_NOVMON,
@@ -367,23 +368,32 @@ declare_resource! {
         all(target_os = "linux", target_env = "gnu"),
         all(target_os = "linux", target_env = "musl", any(target_arch = "x86", target_arch = "mips", target_arch = "powerpc", target_arch = "hexagon", target_arch = "arm")),
         all(target_os = "linux", target_env = "musl", any(target_arch = "x86_64", target_arch = "aarch64", target_arch = "mips64", target_arch = "powerpc64")),
-        all(target_env = "uclibc",any(target_arch = "mips", target_arch = "mips64"))
+        all(target_os = "linux", target_env = "uclibc", any(target_arch = "mips", target_arch = "mips64")),
     ))]
     /// A limit on the number of extant process (or, more precisely on Linux, threads)
     /// for the real user ID of the calling process.
     NPROC = 13 => RLIMIT_NPROC,
 
     #[cfg(any(
-        target_os = "freebsd"
+        target_os = "freebsd",
     ))]
     /// The maximum number of pseudo-terminals this user id is allowed to create.
     NPTS = 14 => RLIMIT_NPTS,
 
     #[cfg(any(
-        target_os = "dragonfly"
+        target_os = "netbsd",
+    ))]
+    /// The maximum number of simultaneous threads (Lightweight
+    /// Processes) for this user id.  Kernel threads and the
+    /// first thread of each process are not counted against this
+    /// limit.
+    NTHR = 15 => RLIMIT_NTHR,
+
+    #[cfg(any(
+        target_os = "dragonfly",
     ))]
     /// The maximum number of POSIX-type advisory-mode locks available to this user.
-    POSIXLOCKS = 15 => RLIMIT_POSIXLOCKS,
+    POSIXLOCKS = 16 => RLIMIT_POSIXLOCKS,
 
     #[cfg(any(
         target_os = "fuchsia",
@@ -395,12 +405,12 @@ declare_resource! {
         all(target_os = "linux", target_env = "gnu"),
         all(target_os = "linux", target_env = "musl", any(target_arch = "x86", target_arch = "mips", target_arch = "powerpc", target_arch = "hexagon", target_arch = "arm")),
         all(target_os = "linux", target_env = "musl", any(target_arch = "x86_64", target_arch = "aarch64", target_arch = "mips64", target_arch = "powerpc64")),
-        all(target_env = "uclibc",any(target_arch = "mips", target_arch = "mips64"))
+        all(target_os = "linux", target_env = "uclibc", any(target_arch = "mips", target_arch = "mips64")),
     ))]
     /// A limit (in bytes)
     /// on the process's resident set
     /// (the number of virtual pages resident in RAM).
-    RSS = 16 => RLIMIT_RSS,
+    RSS = 17 => RLIMIT_RSS,
 
     #[cfg(any(
         target_os = "fuchsia",
@@ -408,31 +418,32 @@ declare_resource! {
         target_os = "emscripten",
         all(target_os = "linux", target_env = "gnu"),
         all(target_os = "linux", target_env = "musl"),
-        target_env = "uclibc"
+        all(target_os = "linux", target_env = "uclibc"),
     ))]
     /// This specifies a ceiling on the real-time priority
     /// that may be set for this process
     /// using `sched_setscheduler(2)` and `sched_setparam(2)`.
-    RTPRIO = 17 => RLIMIT_RTPRIO,
+    RTPRIO = 18 => RLIMIT_RTPRIO,
 
     #[cfg(any(
         target_os = "fuchsia",
         all(target_os = "linux", target_env = "gnu"),
-        all(target_os = "linux", target_env = "musl")
+        all(target_os = "linux", target_env = "musl"),
     ))]
     /// A limit (in microseconds) on the amount of CPU time
     /// that a process scheduled under a real-time scheduling policy
     /// may consume without making a blocking system call.
-    RTTIME = 18 => RLIMIT_RTTIME,
+    RTTIME = 19 => RLIMIT_RTTIME,
 
     #[cfg(any(
-        any(target_os = "freebsd", target_os = "dragonfly")
+        any(target_os = "freebsd", target_os = "dragonfly"),
+        target_os = "netbsd",
     ))]
     /// The maximum size (in bytes) of socket buffer usage for
     /// this user. This limits the amount of network memory, and
     /// hence the amount of mbufs, that this user may hold at any
     /// time.
-    SBSIZE = 19 =>  RLIMIT_SBSIZE,
+    SBSIZE = 20 => RLIMIT_SBSIZE,
 
     #[cfg(any(
         target_os = "fuchsia",
@@ -440,12 +451,12 @@ declare_resource! {
         target_os = "emscripten",
         all(target_os = "linux", target_env = "gnu"),
         all(target_os = "linux", target_env = "musl"),
-        target_env = "uclibc"
+        all(target_os = "linux", target_env = "uclibc"),
     ))]
     /// A limit on the number
     /// of signals that may be queued
     /// for the real user ID of the calling process.
-    SIGPENDING = 20 => RLIMIT_SIGPENDING,
+    SIGPENDING = 21 => RLIMIT_SIGPENDING,
 
     #[cfg(any(
         target_os = "fuchsia",
@@ -457,32 +468,32 @@ declare_resource! {
         target_os = "emscripten",
         all(target_os = "linux", target_env = "gnu"),
         all(target_os = "linux", target_env = "musl"),
+        all(target_os = "linux", target_env = "uclibc"),
         target_os = "solarish",
-        target_env = "uclibc"
     ))]
     /// The maximum size (in bytes)
     /// of the process stack.
-    STACK = 21 => RLIMIT_STACK,
+    STACK = 22 => RLIMIT_STACK,
 
     #[cfg(any(
-        target_os = "freebsd"
+        target_os = "freebsd",
     ))]
     /// The maximum size (in bytes) of the swap space that may be
     /// reserved or used by all of this user id's processes.
-    SWAP = 22 => RLIMIT_SWAP,
+    SWAP = 23 => RLIMIT_SWAP,
 
     #[cfg(any(
-        target_os = "freebsd"
+        target_os = "freebsd",
     ))]
     /// The number of shared locks a given user may create simultaneously.
-    UMTXP = 23 =>  RLIMIT_UMTXP,
+    UMTXP = 24 => RLIMIT_UMTXP,
 
     #[cfg(any(
         any(target_os = "freebsd", target_os = "dragonfly"),
-        target_os = "solarish"
+        target_os = "solarish",
     ))]
     /// An alias for RLIMIT_AS. The maximum size of a process's mapped address space in bytes.
-    VMEM = 24 => RLIMIT_VMEM,
+    VMEM = 25 => RLIMIT_VMEM,
 }
 
 impl Resource {
