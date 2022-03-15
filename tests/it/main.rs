c1c35eb6eab37558e132dafc9a9ca1f419e480af
Nugine <nugine@foxmail.com>
2022-03-15 09:51:38 +0800

    update tests

--- tests/it/main.rs
+++ tests/it/main.rs
@@ -2,21 +2,21 @@
 mod linux;
 
 #[cfg(unix)]
-mod rlim;
-
-#[cfg(unix)]
 mod unix;
 
 #[cfg(windows)]
 mod windows;
 
+#[allow(unused)]
 use std::io;
 
+#[allow(unused)]
 #[track_caller]
 fn expect_ok(result: io::Result<()>) {
     assert!(result.is_ok());
 }
 
+#[allow(unused)]
 #[track_caller]
 fn expect_err(result: io::Result<()>, kind: io::ErrorKind) {
     assert_eq!(result.unwrap_err().kind(), kind);
