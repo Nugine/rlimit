0bb105142d11af8dbe5d23979b6a0254e33ebf88
Nugine <nugine@foxmail.com>
2024-12-17 15:45:31 +0800

    codegen: ignore `linux_time_bits64`

--- codegen/src/main.rs
+++ codegen/src/main.rs
@@ -4,8 +4,30 @@ mod bindings;
 mod build;
 mod resource;
 
-use codegen_libc::{search, CfgItem, RegexSet};
+use codegen_cfg::ast::{Pred, Var};
+use codegen_cfg::bool_logic::ast::Expr;
+use codegen_cfg::bool_logic::visit_mut::{walk_mut_expr, VisitMut};
+use codegen_libc::{search, simplified_expr, CfgItem, RegexSet};
 use codegen_writer::{scoped, Codegen};
+
+fn patch_cfg_expr(expr: &mut Expr<Pred>) {
+    struct Visitor;
+
+    impl VisitMut<Pred> for Visitor {
+        fn visit_mut_expr(&mut self, expr: &mut Expr<Pred>) {
+            walk_mut_expr(self, expr);
+
+            if let Expr::Var(Var(Pred { key, value: None })) = expr {
+                // ignore `linux_time_bits64`
+                if key.as_str() == "linux_time_bits64" {
+                    *expr = Expr::Const(true)
+                }
+            }
+        }
+    }
+
+    Visitor.visit_mut_expr(expr);
+}
 
 fn collect_item_list() -> Vec<CfgItem> {
     let libc_path = "temp/libc";
@@ -18,7 +40,15 @@ fn collect_item_list() -> Vec<CfgItem> {
         "^sysctl$",
     ])
     .unwrap();
-    search(libc_path, &re).unwrap()
+
+    let mut item_list = search(libc_path, &re).unwrap();
+
+    for item in &mut item_list {
+        patch_cfg_expr(&mut item.cfg);
+        item.cfg = simplified_expr(item.cfg.clone());
+    }
+
+    item_list
 }
 
 fn main() {
